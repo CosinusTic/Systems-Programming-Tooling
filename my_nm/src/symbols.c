8b9ce667188b0bcf7cc7bd63d9bf1baa5adc0ad2
nathan.delmarche <nathan.delmarche@epita.fr>
2024-12-03 22:50:57 +0100

    FEAT: traversal function (untested)

--- my_nm/src/symbols.c
+++ my_nm/src/symbols.c
@@ -1,6 +1,7 @@
 #include "include/symbols.h"
 
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 struct sym_llist *create_node(struct sym_data *data)
 {
@@ -48,4 +49,36 @@ void free_list(struct sym_llist *head)
     }
 
     free(head);
+}
+
+static void print_symbol(struct sym_data *data)
+{
+    /*
+    struct sym_data
+    {
+        u_int64_t value;
+        u_int64_t size;
+        char *type;
+        char *binding;
+        char *visibility;
+        char *section_name;
+        char *symbol_name;
+    };*/
+    printf("%016lx\t%lu\t%-12s\t%-12s\t%-12s\t%s\t%s\n", data->value,
+           data->size, data->type, data->binding, data->visibility,
+           data->section_name, data->symbol_name);
+}
+
+void sym_llist_traversal(struct sym_llist *head)
+{
+    if (!head)
+        return;
+
+    struct sym_llist *current = head;
+    print_symbol(head->data);
+    while (current->next)
+    {
+        print_symbol(current->next->data);
+        current = current->next;
+    }
 }
