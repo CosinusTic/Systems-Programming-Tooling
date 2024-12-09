3e9f6887724a884b7db805b0a1b71a9996bb09ed
nathan.delmarche <nathan.delmarche@epita.fr>
2024-12-09 21:09:42 +0100

    FEAT: exit status

--- my_strace/src/main.c
+++ my_strace/src/main.c
@@ -33,9 +33,15 @@ int main(int argc, char *argv[], char *envp[])
 
     while (1)
     {
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
+        pid_t w = waitpid(pid, &status, 0);
+        if (w == -1)
+            perror("Error on waitpid");
+        if (WIFEXITED(status)) // Exit from child program
+        {
+            int stat = WEXITSTATUS(status);
+            printf("Program terminated with exit code %d\n", stat);
             break;
+        }
 
         pt = ptrace(PTRACE_PEEKUSER, pid, 8 * ORIG_RAX, NULL);
         ptrace(PTRACE_GETREGS, pid, NULL, &regs);
@@ -44,8 +50,6 @@ int main(int argc, char *argv[], char *envp[])
 
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
     }
-
-    printf("Program exited with exit code %d\n", status);
 
     return 0;
 }
