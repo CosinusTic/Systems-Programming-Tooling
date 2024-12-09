c62168ec879cdee58e173508f51e551518300e91
nathan.delmarche <nathan.delmarche@epita.fr>
2024-12-09 20:43:00 +0100

    FEAT: base code from course + mapping of syscall table

---
+++ my_strace/src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/syscall.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "include/syscalls.h"
+
+int main(int argc, char *argv[], char *envp[])
+{
+    if (argc != 2)
+        return 1;
+
+    int status = 0;
+    long pt = 0;
+    struct user_regs_struct regs = { 0 };
+    int pid = fork();
+
+    switch (pid)
+    {
+    case -1:
+        return 2;
+
+    case 0:
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execve(argv[1], argv + 1, envp);
+
+    default:
+        break;
+    }
+
+    while (1)
+    {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+            break;
+
+        pt = ptrace(PTRACE_PEEKUSER, pid, 8 * ORIG_RAX, NULL);
+        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+        printf("syscall: %ld: %s\nrdi: %lld\nrsi: %lld\nrdx: %lld\n\n", pt,
+               syscallnames[pt], regs.rdi, regs.rsi, regs.rdx);
+
+        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+    }
+
+    printf("Program exited with exit code %d\n", status);
+
+    return 0;
+}
